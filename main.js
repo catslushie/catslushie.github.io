cdde8ac765b5d6b70b1646e3f993585d1a43497c
Zara Shahzad <ZaraShahzad76@gmail.com>
2024-03-18 21:28:32 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 var mousePressed = false;
+var touchDrawing = false;
 const MAIN_MOUSE_BUTTON = 0;
 
 canvas.addEventListener("mousedown", mouseDown);
 canvas.addEventListener("mouseup", mouseUp);
 canvas.addEventListener("mousemove", draw);
-
+canvas.addEventListener("touchstart", startDrawing);
+canvas.addEventListener("touchend", endDrawing);
+canvas.addEventListener("touchmove", drawTouch);
 
 function setlineprops() {
     ctx.lineWidth = 4;
@@ -39,4 +42,35 @@ function draw(){
     ctx.lineTo(x, y); // Draw a line to the current mouse position
     ctx.stroke(); // Stroke the current path
 
+}
+
+function startDrawing() {
+    event.preventDefault();
+    touchDrawing = true;
+    ctx.lineTo(x,  y);
+    ctx.stroke();
+}
+
+function endDrawing() {
+    touchDrawing = false;
+    ctx.beginPath();
+}
+
+function drawTouch() {
+    if(!mousePressed) return;
+
+    const { x, y } = getCanvasCoordinates(event.touches[0]);
+    ctx.lineTo(x, y); // Draw a line to the current mouse position
+    ctx.stroke(); // Stroke the current path
+
+}
+
+function getCanvasCoordinates(event) {
+    const rect = canvas.getBoundingClientRect();
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
+    return {
+        x: (event.pageX - rect.left) * scaleX,
+        y: (event.pageY - rect.top) * scaleY
+    }
 }
